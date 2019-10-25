3d44cbb39c607522bd6cb16386a34b15c2260e12
ojrahn <olivia.rahn@gmail.com>
2019-10-24 21:50:18 -0700

    added dataframe with julian days for dates collected, added into glmer for flowering data

--- Scripts/lewisii_data_summer_2019.R
+++ Scripts/lewisii_data_summer_2019.R
@@ -1,4 +1,4 @@
-##################################################Density and Distance from 2016 from averages######################################
+##################################################Density and Distance from 2016######################################
 
 density_df2 <- merge(density_df,lewisii_data, by.x = 2, by.y = 0, all.x = TRUE)
 
@@ -10,13 +10,13 @@ geom_point()+geom_smooth(method = "lm")+ labs(x="Distance from 2016 Line (m)", y
 #install.packages("standardize")
 library(standardize)
 
-#standardizing predictor variables - don't need this
-#density_df$distance_scaled <- scale(density_df$distance_from_2016, center=TRUE)[,1]
+#standardizing predictor variables
+density_df2$distance_scaled <- scale(density_df2$distance_from_2016, center=TRUE)[,1]
 #density_df$elev_scaled <- scale(density_df$elev, center=TRUE)[,1]
 
 #making model
 #log of plants to make residual plot better
-model_density_distance <- lmer(log(Number_of_Plants) ~ distance_from_2016 + (1|Foreland_Code.x), data=density_df2)
+model_density_distance <- lmer(log(Number_of_Plants) ~ distance_scaled + (1|Foreland_Code.x), data=density_df2)
 
 summary(model_density_distance)
 anova(model_density_distance)
@@ -27,8 +27,8 @@ vis_density_distance <- visreg(
   model_density_distance,
   main = "Predicted Population Density vs. Distance from 2016 Glacial
   Terminus",
-  ylab = "Predicted Population Density",
-  xlab = "Distance from 2016 Terminus",
+  ylab = "log(Predicted Population Density)",
+  xlab = "Scaled Distance from 2016 Terminus",
   gg = TRUE,
   line.par = list(col = 'orange'),
   points.par = list(col = 'red', cex = 1, pch =
@@ -47,9 +47,9 @@ avg_stemcount_ns$timezone <- timezones
 avg_stemcount_ns$numerical_timezones <- timezones_numerical
 avg_stemcount_ns$number_of_seedlings <- number_of_seedlings
 
-Foreland_Code <- avg_stemcount$Foreland_Code
 
 avg_stemcount_ns$distance_from_2016 <- avg_stemcount_ns$Distance_from_Glacial_Terminus_.m.
+avg_stemcount_ns$Foreland_Code <- avg_stemcount_ns$Foreland_Code
 
 ###scatterplot for stem count and distance
 stemcount_distance_scatter <-
@@ -58,12 +58,13 @@ stemcount_distance_scatter <-
   "Average Stem Count", title = "Stem Count vs. Distance from 2016")
 
 #scaling predictor variables - don't need
-#avg_stemcount_ns$distance_scaled <- scale(avg_stemcount_ns$distance_from_2016, center=TRUE)[,1]
+avg_stemcount_ns$distance_scaled <- scale(avg_stemcount_ns$distance_from_2016, center=TRUE)[,1]
 #avg_stemcount_ns$elev_scaled <- scale(avg_stemcount$elev, center=TRUE)[,1]
+avg_stemcount_ns$stemcount_scaled <- scale(avg_stemcount_ns$SC_ns, center=TRUE)[,1]
 
-###model for stem count and distance (from averages)############
+#######model for stem count and distance (from averages)############
 
-model_stemcount_distance <- lmer(SC_ns ~ distance_from_2016 + (1|Foreland_Code),data=avg_stemcount_ns)
+model_stemcount_distance <- lmer(SC_ns ~ distance_scaled + (1|Foreland_Code),data=avg_stemcount_ns)
 
 summary(model_stemcount_distance)
 anova(model_stemcount_distance)
@@ -83,7 +84,7 @@ vis_stemcount_distance <- visreg(model_stemcount_distance,
 
 #####################model for stemcount and distance (from main dataframe)##################
 
-model_stemcount_main <- lmer(Stem_Count ~ Distance_from_Glacial_Terminus_.m. + (1|Foreland_Code/Site),data=df_no_seedlings)
+model_stemcount_main <- lmer(Stem_Count ~ distance_scaled + (1|Foreland_Code),data=df_no_seedlings)
 summary(model_stemcount_main)
 anova(model_stemcount_main)
 
@@ -101,7 +102,7 @@ vis_stemcount_main <- visreg(
                       'red', cex = 1, pch = 1)
 )
 resid_plot_stemcount <- plot(model_stemcount_main)
-#possibly overrepresented on lower end? 
+
 
 #########################################Proportion Flowering and Distance from 2016##############
 
@@ -125,18 +126,33 @@ flowering_distance_scatter <- ggplot(prop_flowering, aes(x = distance_from_2016,
 prop_flowering_ns$distance_scaled <- scale(prop_flowering_ns$distance_from_2016, center=TRUE)[,1]
 #prop_flowering_ns$elev_scaled <- scale(prop_flowering_ns$elev, center=TRUE)[,1]
 
+model_flowering_av <- lmer(prop_f_site ~ distance_scaled+ (1|Foreland_Code), data=prop_flowering_ns)
+
 ########################model for flowering and distance (from main dataframe)############
 
 #change flowering to factor
 df_no_seedlings$flowering_factor <- as.factor(df_no_seedlings$flowering_numerical)
+#change flowering to factor in dataframe with julian days
+lewisii_data2$flowering_factor <- as.factor(lewisii_data2$flowering_numerical)
 
 #standardize predictor variable
 library(standardize)
-df_no_seedlings$distance_scaled <- scale(df_no_seedlings$dist)
+df_no_seedlings$distance_scaled <- scale(df_no_seedlings$dist, center=TRUE)[,1]
+#doing this for julian days dataframe (using this instead to include date)
+lewisii_data2$distance_scaled <- scale(lewisii_data2$dist, center=TRUE)[,1]
+lewisii_data2$jday <- lewisii_data2$Julian.Day
 
-model_flowering_main <-glmer(flowering_factor ~ distance_scaled + (1|Foreland_Code), data = df_no_seedlings, family = "binomial")
+#install.packages("optimx")
+library(optimx)
+
+model_flowering_main <- glmer(flowering_factor ~ distance_scaled +(1|Foreland_Code)+(1|jday), data = lewisii_data2, family = "binomial")
+#optimizer code?
+control = glmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "nlminb", starttests = FALSE,kkt = FALSE))
+                              
 summary(model_flowering_distance)
 anova(model_flowering_distance)
+resid_plot_flowering <- plot(model_flowering_main)
+
 vis_model_flowering <- visreg(
   model_flowering_main,
   main = "Predicted Average Stem Count vs. Distance from 2016  Glacial
@@ -152,9 +168,10 @@ vis_model_flowering <- visreg(
 #looking at residuals
 modplot_f <- plot(model_flowering_distance)
 #looks okay
-#glm instead of glmer
-model_flowering_glm <- glm(flowering_factor ~ dist, data=df_no_seedlings, family="binomial")
-visflowering_glm <- visreg(model_flowering_glm)
+#glm instead of glmer (don't use)
+#model_flowering_glm <- glm(flowering_factor ~ dist, data=df_no_seedlings, family="binomial")
+#summary(model_flowering_glm)
+#visflowering_glm <- visreg(model_flowering_glm)
 
 
 ########################model for flowering and distance from averages############
@@ -175,6 +192,7 @@ anova(model_floweringGB_av)
 visreg(model_floweringGB_av)
 
 #for both
+prop_flowering_ns$flowering_scaled <- scale(prop_flowering_ns$prop_f_site)
 model_flowering_distance <-  lmer(prop_f_site ~ distance_scaled + (1|Foreland_Code), data=prop_flowering_ns)
 summary(model_flowering_distance)
 anova(model_flowering_distance)
@@ -185,7 +203,7 @@ anova(model_flowering_lm)
 
 ####visreg code
 vis_flowering_distance <- visreg(
-  model_flowering_distance,
+  model_flowering_lm,
   main = "Predicted Proportion of Flowering Plants vs. Distance from 2016 Glacial
   Terminus",
   ylab = "Predicted Proportion of Flowering Plants",
@@ -222,38 +240,35 @@ library(ggplot2)
 #prop_flowering$distance_scaled <- scale(prop_flowering$distance_from_2016, center=TRUE)[,1]
 #prop_flowering$elev_scaled <- scale(prop_flowering$elev, center=TRUE)[,1]
 
-model_seedlings_distance <- glmer(number_of_seedlings ~ distance_from_2016 + (1|Foreland_Code), data=prop_flowering)
+model_seedlings_distance <- lmer(number_of_seedlings ~ distance_from_2016 + (1|Foreland_Code), data=prop_flowering)
+
 summary(model_seedlings_distance)
 anova(model_seedlings_distance)
 #looking at residuals
 modplot_s <- plot(model_seedlings_distance)
 #looks okay
 
-#make seedlings model with data from main dataframe
+########################make seedlings model with data from main dataframe####################
 
 #make seedling (0 stem count) a "Y" "N" binary in new column
 lewisii_data$seedling <- ifelse(lewisii_data$Stem_Count=='0', 1,0)
 lewisii_data$seedling_factor <- as.factor(lewisii_data$seedling)
-#scale distance
-#lewisii_data$distance_scaled <- scale(lewisii_data$dist)
 
 #model
 #scale distance
-lewisii_data$distance_scaled <- scale(lewisii_data$Distance_from_Glacial_Terminus_.m.)
+lewisii_data$distance_scaled <- scale(lewisii_data$Distance_from_Glacial_Terminus_.m.,center=TRUE)[,1]
 #make model
 model_seedlings_main <- glmer(seedling_factor ~ distance_scaled + (1|Foreland_Code), data = lewisii_data, family = "binomial")
 summary(model_seedlings_main)
 anova(model_seedlings_main)
+resid_plot_seedlings <- plot(model_seedlings_main)
+
 #use glm instead of glmer
 fit = glm(seedling ~ distance_scaled, data=lewisii_data, family=binomial)
 newdatseedlings <- data.frame(dist=seq(min(lewisii_data$distance_scaled), max(lewisii_data$distance_scaled),len=3390))
 newdatseedlings$seedling = predict(fit, newdata1=newdatseedlings, type="response")
 plot(seedling ~ distance_scaled, data=lewisii_data, col="red4")
 lines(seedling ~ lewisii_data$distance_scaled, newdatseedlings, col="green4", lwd=2)
-
-####make histogram w/seedlings and distance from terminus
-subset_seedlings <- subset(lewisii_data,seedling==1,select=dist)
-hist(as.numeric(subset_seedlings))
 
 #visreg
 vis_seedlings_main <- visreg(
@@ -269,43 +284,12 @@ vis_seedlings_main <- visreg(
 
 ####################################putting models on one page######################################################
 
-#multiplot function
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  require(grid)
-  
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))}}}
-      
-      
-#all models on one page
-allmodels <- multiplot(vis_density_distance,vis_flowering_distance,vis_seedlings_distance,vis_stemcount_distance, cols=2)
+library(gridExtra)
+gridmodels <- grid.arrange(vis_density_distance,vis_flowering_main,vis_stemcount_main,vis_seedlings_main, ncol=2)
+
+###residuals plots
+
+gridresiduals <- grid.arrange(resid_plot_distance,resid_plot_flowering,resid_plot_seedlings,resid_plot_stemcount)
 
                         
 ##########notes ect#####################
